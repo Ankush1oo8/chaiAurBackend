cc73b94f215bd0ecd8935bfc0f9da5f153fad36e
Ankush1oo8 <154556772+Ankush1oo8@users.noreply.github.com>
2024-12-11 01:24:47 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// require ("dotenv").config(); this can cause code to look bad
+import dotenv from "dotenv"
+import express from "express"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'/.env'
+})
+
+
+
+
+connectDB();
