93de3f4ffb379ef005f666aedec94b1fb0327ace
Ankush1oo8 <154556772+Ankush1oo8@users.noreply.github.com>
2024-12-14 14:00:41 +0530

    export loginUser and logoutUser

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,25 @@ import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 
+
+const generateAccessAndRefreshTokens = async(userId)=>
+{
+    try {
+        const user =await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken=refreshToken;
+        await user.save({validateBeforeSave : false});
+        
+        return {accessToken,refreshToken};
+
+    } catch (error) {
+        throw new ApiError(500,"Something went wrong while generating Access and Refresh Tokens!!")
+    }
+}
+
+
 const registerUser=asyncHandler(async (req,res)=>{
     const {fullname, email, username, password}=req.body
     // console.log("email: ",email);
@@ -62,4 +81,55 @@ const registerUser=asyncHandler(async (req,res)=>{
     )
 })
 
-export {registerUser}
+const loginUser = asyncHandler(async(req,res)=>{
+    const { username, email, password }=req.body;
+
+    if(!(username || email)){
+        throw new ApiError(400,"Username or email is required!!!")
+    }
+
+    const user = await User.findOne(
+        {
+            $or:[{username},{email}]
+        }
+    )
+
+    if(!user){
+        throw new ApiError(404,"No user found with this credentials")
+    }
+
+    const validUser = await user.isPasswordCorrect(password)
+
+    if(!validUser){
+        throw new ApiError(401,"Invalid User Credentials")
+    }
+
+    const {accessToken,refreshToken} = await generateAccessAndRefreshTokens(user._id);
+
+    const loggedInUser=await User.findById(user._id).select("-password -refreshToken");
+
+    const options = {
+        httpOnly:true,
+        secure:true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,option)
+    .json(
+        200,
+        {
+            user:loggedInUser, accessToken,refreshToken
+        }
+        "User has loggedIn successfully"
+    )
+})
+
+
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser
+    }
