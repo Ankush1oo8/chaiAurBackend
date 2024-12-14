b048e6bc602ac1b8f93324cb38636d0a80048213
Ankush1oo8 <154556772+Ankush1oo8@users.noreply.github.com>
2024-12-15 00:13:14 +0530

    typos

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const subscriptionSchema = new mongoose.Schema({
     subscriber:{
-        types:mongoose.Schema.Types.ObjectId;
+        types:mongoose.Schema.Types.ObjectId,
         ref:"User"
     },
     channel:{
-        types:mongoose.Schema.Types.ObjectId;
+        types:mongoose.Schema.Types.ObjectId,
         ref:"User"
     }
 },{
